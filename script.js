b9ccc379c74962862022ef90dd3a871d0e752883
PedroM-K <pedromoreiralonga999@gmail.com>
2025-09-01 20:29:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 let display = document.getElementById('display');
 
 function appendNumber(num) {
-    display.value += num;
+  display.value += num;
 }
 
 function appendOperator(op) {
-    display.value += op;
+  display.value += op;
 }
 
 function appendFunction(func) {
-    display.value += func;
+  display.value += func;
 }
 
 function clearDisplay() {
-    display.value = '';
+  display.value = '';
 }
 
 function deleteLast() {
-    display.value = display.value.slice(0, -1);
+  display.value = display.value.slice(0, -1);
+}
+
+// Função de fatorial
+function factorial(n) {
+  if (n < 0) return NaN;
+  if (n === 0 || n === 1) return 1;
+  return n * factorial(n - 1);
 }
 
 function calculate() {
-    try {
-        // Substitui "^" por Math.pow()
-        let expression = display.value.replace(/(\d+)\^(\d+)/g, 'Math.pow($1,$2)');
-        display.value = eval(expression);
-    } catch {
-        display.value = 'Erro';
-    }
+  try {
+    let expression = display.value;
+
+    // Substitui "xʸ" (Math.pow) se o usuário digitar ^
+    expression = expression.replace(/(\d+)\^(\d+)/g, 'Math.pow($1,$2)');
+
+    display.value = eval(expression);
+  } catch {
+    display.value = 'Erro';
+  }
 }
